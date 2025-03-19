33d5f6bff7eaef992bfecbe463e1c90148da0760
Raymond Chiu <raymondchiu10@gmail.com>
2025-03-19 10:50:16 -0700

    create warehouse post endpoint, added validator library

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,17 +1,71 @@
 import express from "express";
 import connection from "../utils/mysql.js";
+import validator from "validator";
 
 const warehousesRouter = express.Router();
 
-warehousesRouter.get("/", async (_req, res) => {
-  const sql = "SELECT * FROM warehouses;";
+warehousesRouter
+	.route("/")
+	.get(async (_req, res) => {
+		const sql = "SELECT * FROM warehouses;";
 
-  try {
-    const [results] = await connection.query(sql);
-    res.json(results);
-  } catch (error) {
-    return res.status(400).send(error);
-  }
-});
+		try {
+			const [results] = await connection.query(sql);
+			res.json(results);
+		} catch (error) {
+			return res.status(400).send(error);
+		}
+	})
+	.post(async (req, res) => {
+		const { warehouse_name, address, city, country, contact_name, contact_position, contact_phone, contact_email } =
+			req.body;
+
+		if (
+			!warehouse_name ||
+			!address ||
+			!city ||
+			!country ||
+			!contact_name ||
+			!contact_position ||
+			!contact_phone ||
+			!contact_email
+		) {
+			return res.status(400).send({ error: "All fields are required." });
+		}
+
+		if (!validator.isMobilePhone(contact_phone, "any", { strictMode: false })) {
+			return res.status(400).json({ error: "Invalid phone number." });
+		}
+
+		if (!validator.isEmail(contact_email)) {
+			return res.status(400).json({ error: "Invalid email." });
+		}
+
+		try {
+			const sql = "INSERT INTO warehouses SET ?";
+
+			const [results] = await connection.query(sql, {
+				warehouse_name,
+				address,
+				city,
+				country,
+				contact_name,
+				contact_position,
+				contact_phone,
+				contact_email,
+			});
+
+			const insertId = results.insertId;
+
+			const sql2 = "SELECT * FROM warehouses WHERE id = ?";
+
+			const [warehouse] = await connection.query(sql2, [insertId]);
+
+			return res.status(201).json(warehouse[0]);
+		} catch (err) {
+			console.error(err);
+			return res.status(400).send();
+		}
+	});
 
 export default warehousesRouter;
