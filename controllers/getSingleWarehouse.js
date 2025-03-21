dc1a39440491f111a7c3681f58da000c16b1c33e
Samrit Basnet <samritbasnet70@gmail.com>
2025-03-21 12:12:02 -0400

    fixed some typos

--- controllers/getSingleWarehouse.js
+++ controllers/getSingleWarehouse.js
@@ -2,14 +2,13 @@ import connection from '../utils/mysql.js';
 
 export const getSingleWareHouse = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
   const sql = ' SELECT  * from warehouses WHERE id=?';
   if (!id) {
     return res.status(404);
   }
   try {
     const [results] = await connection.query(sql, [id]);
-    console.log(results);
+    
     if (results.length < 1) {
       return res.sendStatus(404);
     }
